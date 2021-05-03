a76add14fce48232164abd68193769ddd3b45472
EnriqueRS <enrique.rios.santos@gmail.com>
2021-05-03 12:18:27 +0200

    custom components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <tittle text="My dinamic Bank 2.0"></tittle>
+  <bank-account></bank-account>
 </template>
 
 <script>
 import Tittle from './components/Tittle'
+import BankAccount from './components/BankAccount'
 
 export default {
   name: 'App',
   components: {
-    Tittle
+    Tittle,
+    BankAccount
   }
 }
 </script>
---
+++ src/components/BankAccount.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Bank Account</h1>
+</template>
+
+<script>
+export default {
+    name: 'BankAccount'
+
+}
+</script>
+
+<style>
+
+</style>
